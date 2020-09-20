228e9cb1e1f78edc03530e73a0c0a2f31530f0b9
eKuG <ekanshgupta.eku@gmail.com>
2020-09-20 19:03:10 +0530

    Some problem in the backend

---
+++ ReactFrontend/src/components/EditorPage/Editor.js
@@ -0,0 +1,30 @@
+import React, { useState} from "react";
+import AceEditor from "react-ace";
+ 
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/theme-github";
+ 
+function Editor() {
+  const [editorText, setEditorText] = useState('');
+  return <AceEditor
+  placeholder="Placeholder Text"
+  mode="javascript"
+  theme="monokai"
+  name="blah2"
+    onChange={(newValue)=>setEditorText(newValue)}
+  width="100%"
+  fontSize={14}
+  showPrintMargin={true}
+  showGutter={true}
+  highlightActiveLine={true}
+  value={editorText}
+  setOptions={{
+  enableBasicAutocompletion: true,
+  enableLiveAutocompletion: true,
+  enableSnippets: true,
+  showLineNumbers: true,
+  tabSize: 2,
+  }}/>
+     
+}
+export default Editor;
---
+++ ReactFrontend/src/components/OutputPage/Output.js
@@ -0,0 +1,89 @@
+import React, {useState} from 'react';
+import { Paper, Typography, Button, Grid,Container } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+    paper: {
+        padding: '6px 16px',
+    },
+    secondaryTail: {
+        backgroundColor: theme.palette.secondary.main,
+    },
+}));
+  
+
+function Output() {
+    const [code, setCode] = useState('');
+    const [output, setOutput] = useState('');
+    const [input, setInput] = useState("/* input here */");
+    
+    const classes = useStyles();
+    return (
+        <div>
+            <Container>
+            <br/>
+            < Typography variant="h2" align="center">
+                Output
+            </Typography>
+            <br/>
+            <Grid
+                row
+                justify="center"
+                alignItems="center"
+            >
+                <Grid item>
+                <Paper elevation={3} className={classes.paper}>
+                    <Typography  variant="h5" component="h1">
+                        Code
+                    </Typography>
+                    <Typography variant="body1" >
+                        {code}
+                    </Typography>
+                </Paper>
+                </Grid>
+                <br />
+                <Grid item>
+                    <Paper elevation={3} className={classes.paper}>
+                        <Typography  variant="h5" component="h1">
+                            Input
+                        </Typography>
+                        <Typography variant="body1" >
+                            {input}
+                        </Typography>
+                    </Paper>
+                </Grid>
+                <br /> 
+                <Grid item>
+                    <Paper elevation={3} className={classes.paper}>
+                        <Typography  variant="h5" component="h1">
+                            Output
+                        </Typography>
+                        <Typography variant="body1" >
+                            {output}
+                        </Typography>
+                    </Paper>
+                </Grid>
+                <br/>     
+                <Grid item justify="flex-end" align="right">
+                <div className={classes.root}>
+                    <Link to="/done">
+                        <Button variant="contained" color="secondary" component="span">
+                            Finish
+                        </Button>
+                    </Link>
+                </div>
+                </Grid>
+            </Grid>
+            <br />
+        </Container>
+        </div>
+    )
+}
+
+export default Output;
